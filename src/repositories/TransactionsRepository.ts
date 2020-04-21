8eec4b42fb15b71609c27bfa7661ec38a2c3cde4
Caroline Carvalho <carvalhocaroline111@gmail.com>
2020-04-21 17:48:09 -0300

    Desafio 5 completo

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface Response {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +24,52 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): Response {
+    const response = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+
+    return response;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balanceTransactions = this.transactions.reduce(
+      (accumulator, current) => {
+        if (current.type === 'income') {
+          return {
+            income: accumulator.income + current.value,
+            outcome: accumulator.outcome,
+          };
+        }
+        return {
+          income: accumulator.income,
+          outcome: accumulator.outcome + current.value,
+        };
+      },
+      { income: 0, outcome: 0 },
+    );
+
+    const balance = {
+      ...balanceTransactions,
+      total: balanceTransactions.income - balanceTransactions.outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (transaction.type === 'outcome') {
+      if (this.getBalance().total < transaction.value) {
+        throw Error('Insufficient balance to carry out transaction.');
+      }
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
